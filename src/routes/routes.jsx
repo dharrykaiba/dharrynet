c04359c9d4f9c25c9ac59eb761f79dee7c0a3233
dharrykaiba <dharrykaiba@gmail.com>
2025-12-09 04:24:00 -0500

    mod router

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,15 +1,16 @@
-//src/routes.jsx
+// src/routes.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import HomeView from "../pages/HomeView";
-import FS25View from "../pages/FS25View";
+import { Routes, Route, Navigate } from "react-router-dom";
 import FS25ViewBuy from "../pages/FS25ViewBuy.jsx";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomeView />} />
-      <Route path="/fs25" element={<FS25View />} />
+      {/* Redirigir '/' y '/fs25' hacia '/fs25buy' */}
+      <Route path="/" element={<Navigate to="/fs25buy" replace />} />
+      <Route path="/fs25" element={<Navigate to="/fs25buy" replace />} />
+
+      {/* Ruta de compra */}
       <Route path="/fs25buy" element={<FS25ViewBuy />} />
     </Routes>
   );
